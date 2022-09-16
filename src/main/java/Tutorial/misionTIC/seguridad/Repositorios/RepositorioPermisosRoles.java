53014cb0cf86f79b431827b30cc751c0509b96a5
Formador <mticpro31fi_bog@unal.edu.co>
2022-09-15 22:55:46 -0500

    first commit

---
+++ src/main/java/Tutorial/misionTIC/seguridad/Repositorios/RepositorioPermisosRoles.java
@@ -0,0 +1,10 @@
+package Tutorial.misionTIC.seguridad.Repositorios;
+import Tutorial.misionTIC.seguridad.Modelos.PermisosRoles;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+public interface RepositorioPermisosRoles extends MongoRepository<PermisosRoles,String>
+{
+
+}
+
