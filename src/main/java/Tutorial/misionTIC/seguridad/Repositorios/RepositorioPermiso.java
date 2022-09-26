96a72336c47fd90e2fba6dcafacbb3bfe4f6f7ca
Formador <mticpro31fi_bog@unal.edu.co>
2022-09-26 16:05:29 -0500

    Repo

--- src/main/java/Tutorial/misionTIC/seguridad/Repositorios/RepositorioPermiso.java
+++ src/main/java/Tutorial/misionTIC/seguridad/Repositorios/RepositorioPermiso.java
@@ -1,7 +1,9 @@
 package Tutorial.misionTIC.seguridad.Repositorios;
 import Tutorial.misionTIC.seguridad.Modelos.Permiso;
 import org.springframework.data.mongodb.repository.MongoRepository;
-
+import org.springframework.data.mongodb.repository.Query;
 
 public interface RepositorioPermiso extends MongoRepository<Permiso,String> {
+    @Query("{'url':?0,'metodo':?1}")
+    Permiso getPermiso(String url, String metodo);
 }
